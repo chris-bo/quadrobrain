c5f508492b2168aac6c632e000f98adeebd0fa2b
hoenas <hoenas@gmail.com>
2015-04-04 20:39:50 +0200

    - ConfigReader kann uint8_t, uint16_t, uint32_t, float und bool aus eeprom laden / in eeprom schreiben - TODO: gesamte Konfiguration aus Status in eeprom schreiben - TODO: gesamte Konfiguration aus eeprom in Status schreiben

--- inc/ConfigReader.h
+++ inc/ConfigReader.h
@@ -12,8 +12,8 @@
 #include "stm32f3xx_hal.h"
 
 // Definitions
-#define EEPROM_ADDRESS		0b10100000
-#define EEPROM_I2C_TIMEOUT	0xFFFFF
+#define EEPROM_ADDRESS	0b10100000
+#define EEPROM_I2C_TIMEOUT		0xFFFFF
 
 
 class ConfigReader {
@@ -22,11 +22,21 @@ public:
 	virtual ~ConfigReader();
 	void loadConfiguration( Status* status );
 	void saveConfiguration( Status* status );
+	void loadVariable( uint8_t* variable, uint16_t address );
+	void loadVariable( uint16_t* variable, uint16_t address );
+	void loadVariable( uint32_t* variable, uint16_t address );
+	void loadVariable( float* variable, uint16_t address );
+	void loadVariable( bool* variable, uint16_t address );
 
+	void saveVariable( uint8_t* variable, uint16_t address );
+	void saveVariable( uint16_t* variable, uint16_t address );
+	void saveVariable( uint32_t* variable, uint16_t address );
+	void saveVariable( float* variable, uint16_t address );
+	void saveVariable( bool* variable, uint16_t address );
 private:
 	I2C_HandleTypeDef* eeprom_i2c;
-	void loadVariable( uint8_t* variable, uint16_t address, uint16_t byteCount );
-	void saveVariable( uint8_t* variable, uint16_t address, uint16_t byteCount );
+	void load( uint8_t* variable, uint16_t address, uint16_t byteCount );
+	void save( uint8_t* variable, uint16_t address, uint16_t byteCount );
 
 };
 
--- src/ConfigReader.cpp
+++ src/ConfigReader.cpp
@@ -15,26 +15,111 @@ ConfigReader::~ConfigReader() {
 	// TODO Auto-generated destructor stub
 }
 
+/*
+ * Loads the quadrocopter configuration from eeprom into status
+ */
 void ConfigReader::loadConfiguration( Status* status ) {
-
-}
-
-void ConfigReader::saveConfiguration( Status* status ) {
-
+	//TODO: Alle Werte aus EEPROM holen und in status speichern
 }
 
 /*
- * Loads byteCount bytes into variable from address
+ * Saves the quadrocopter configuration from status into eeprom
  */
-void ConfigReader::loadVariable( uint8_t* variable, uint16_t address, uint16_t byteCount ) {
+void ConfigReader::saveConfiguration( Status* status ) {
+	//TODO: Alle Werte aus status in EEPROM schreiben
+}
+
+/*
+ * Loads a single uint8_t from eeprom
+ */
+void ConfigReader::loadVariable( uint8_t* variable, uint16_t address ) {
+	load( variable, address, 1);
+}
+
+/*
+ * Loads a single uint16_t from eeprom
+ */
+void ConfigReader::loadVariable( uint16_t* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	load( tmp, address, 2);
+}
+
+/*
+ * Loads a single uint32_t from eeprom
+ */
+void ConfigReader::loadVariable( uint32_t* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	load( tmp, address, 4);
+}
+
+/*
+ * Loads a single float from eeprom
+ */
+void ConfigReader::loadVariable( float* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	load( tmp, address, 32);
+}
+
+/*
+ * Loads a single bool from eeprom
+ */
+void ConfigReader::loadVariable( bool* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	load( tmp, address, 1);
+}
+
+/*
+ * Saves a single uint8_t to eeprom
+ */
+void ConfigReader::saveVariable( uint8_t* variable, uint16_t address ) {
+	save( variable, address, 1);
+}
+
+/*
+ * Saves a single uint16_t to eeprom
+ */
+void ConfigReader::saveVariable( uint16_t* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	save( tmp, address, 2);
+}
+
+/*
+ * Saves a single uint32_t to eeprom
+ */
+void ConfigReader::saveVariable( uint32_t* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	save( tmp, address, 4);
+}
+
+/*
+ * Saves a single float to eeprom
+ */
+void ConfigReader::saveVariable( float* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	save( tmp, address, 32);
+}
+
+/*
+ * Saves a single bool to eeprom
+ */
+void ConfigReader::saveVariable( bool* variable, uint16_t address ) {
+	uint8_t* tmp = (uint8_t*)variable;
+	save( tmp, address, 1);
+}
+
+
+/*
+ * Loads byteCount bytes from address into variable
+ */
+void ConfigReader::load( uint8_t* variable, uint16_t address, uint16_t byteCount ) {
 	// Bytes aus I2C-EEPROM holen
-	HAL_I2C_Mem_Read( eeprom_i2c, EEPROM_ADDRESS, address, I2C_MEMADD_SIZE_8BIT, variable, byteCount, EEPROM_I2C_TIMEOUT );
+	HAL_I2C_Mem_Read( eeprom_i2c, EEPROM_ADDRESS, address, I2C_MEMADD_SIZE_16BIT, variable, byteCount, EEPROM_I2C_TIMEOUT );
 }
 
 /*
  * Writes byteCount bytes from variable into address
  */
-void ConfigReader::saveVariable( uint8_t* variable, uint16_t address, uint16_t byteCount ) {
+void ConfigReader::save( uint8_t* variable, uint16_t address, uint16_t byteCount ) {
 	// Bytes in I2C-EEPROM schreiben
-	HAL_I2C_Mem_Write( eeprom_i2c, EEPROM_ADDRESS, address, I2C_MEMADD_SIZE_8BIT, variable, byteCount, EEPROM_I2C_TIMEOUT );
+	HAL_I2C_Mem_Write( eeprom_i2c, EEPROM_ADDRESS, address, I2C_MEMADD_SIZE_16BIT, variable, byteCount, EEPROM_I2C_TIMEOUT );
 }
